03cb7da11cd72cf9dd61fdee3aed670f10943776
Hyunjae-Lee <leehj8687@gmail.com>
2020-03-01 15:27:29 +0900

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          abc
+          HI~ this is a deployed app by HJ
         </p>
         <a
           className="App-link"
