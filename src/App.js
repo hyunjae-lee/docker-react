669da1d3f9eaba3afcf63f756c64e21328e4c9ef
Hyunjae-Lee <leehj8687@gmail.com>
2020-03-01 15:22:37 +0900

    changed app text

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HI this is a deployed app by HJ
         </a>
       </header>
     </div>
